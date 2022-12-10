f7a3146cf3b8a5149673a2d0f34aedb52c7c319e
alexx181207 <alexxge181207@gmail.com>
2022-12-09 23:31:37 -0500

    Actualizaciones

--- Taller/static/js/table.js
+++ Taller/static/js/table.js
@@ -1,35 +1,27 @@
+function createTable(id) {
+  if ($(id).length) {
+    $(id).DataTable({
+      dom: "Blfrtip",
+      buttons: [{
+        extend: "copy",
+        className: "btn-primary"
+      }, {
+        extend: "csv",
+        className: "btn-primary"
+      }, {
+        extend: "excel",
+        className: "btn-primary"
 
-function createTable(id){
-	if ($(id).length) {
-  $(id).DataTable({
-    dom: "Blfrtip",
-    buttons: [
-    {
-      extend: "copy",
-      className: "btn-primary"
-    },
-    {
-      extend: "csv",
-      className: "btn-primary"
-    },
-    {
-      extend: "excel",
-      className: "btn-primary"
-
-    },
-    {
-      extend: "pdfHtml5",
-      className: "btn-primary"
-    },
-    {
-      extend: "print",
-      className: "btn-primary",
-    },
-    ],
-    responsive: false
-  });
+      }, {
+        extend: "pdfHtml5",
+        className: "btn-primary"
+      }, {
+        extend: "print",
+        className: "btn-primary",
+      }, ],
+      responsive: false
+    });
   };
 
 
 }
-
